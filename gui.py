02253faa3f0f8f92981c70d7955c171c9b71067f
Syrnik Konrad <syrnikkonrad@gmail.com>
2022-04-07 15:35:36 +0200

    Add stock prices ploting functionality

--- gui.py
+++ gui.py
@@ -1,7 +1,10 @@
 import sys
-from PyQt5 import QtWidgets, QtGui
-from PyQt5.QtWidgets import QApplication, QMainWindow
+import yfinance as yf
+import plotly.graph_objs as go
+from PyQt5 import QtWidgets, QtGui, QtWebEngineWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow, QSystemTrayIcon
 from PyQt5.uic import loadUi
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -16,24 +19,57 @@ class MainWindow(QMainWindow):
     def go_to_analyse_crypto(self):
         widget.setCurrentIndex(widget.currentIndex()+2)
 
-
 class AnalyseStocks(QMainWindow):
     def __init__(self):
         super().__init__()
         loadUi("static/analyse_stocks.ui", self)
         self.back_button.clicked.connect(self.go_to_main_window)
+        self.browser = QtWebEngineWidgets.QWebEngineView(self)
+        self.vlayout.addWidget(self.browser)
+        self.show_plot()
+
 
     def go_to_main_window(self):
         widget.setCurrentIndex(widget.currentIndex()-1)
+
+    def show_plot(self):
+        stock = 'TSLA'
+        data = yf.download(stock,'2020-01-01')
+        data.reset_index(inplace=True)
+
+        fig = go.Figure()
+        fig.add_trace(go.Candlestick(x=data['Date'], open=data['Open'], close=data['Close'], low=data['Low'], high=data['High']))
+        #fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name='Open'))
+        #fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name='Close'))
+        #fig.add_trace(go.Scatter(x=data['Date'], y=data['Low'], name='Low'))
+        #fig.add_trace(go.Scatter(x=data['Date'], y=data['High'], name='High'))
+        fig.layout.update(title_text=stock, xaxis_rangeslider_visible=True)
+
+        self.browser.setHtml(fig.to_html(include_plotlyjs='cdn'))
 
 class AnalyseCrypto(QMainWindow):
     def __init__(self):
         super().__init__()
         loadUi("static/analyse_crypto.ui", self)
         self.back_button.clicked.connect(self.go_to_main_window)
+        self.browser = QtWebEngineWidgets.QWebEngineView(self)
+        self.vlayout.addWidget(self.browser)
+        self.show_plot()
 
     def go_to_main_window(self):
         widget.setCurrentIndex(widget.currentIndex() - 2)
+
+    def show_plot(self):
+        stock = 'ETH-USD'
+        data = yf.download(stock,'2020-01-01')
+        data.reset_index(inplace=True)
+
+        fig = go.Figure()
+        fig.add_trace(go.Candlestick(x=data['Date'], open=data['Open'], close=data['Close'], low=data['Low'], high=data['High']))
+        fig.layout.update(title_text=stock, xaxis_rangeslider_visible=True)
+
+        self.browser.setHtml(fig.to_html(include_plotlyjs='cdn'))
+
 
 if __name__=="__main__":
     app = QApplication(sys.argv)
@@ -43,9 +79,14 @@ if __name__=="__main__":
     analyse_stocks_window = AnalyseStocks()
     analyse_crypto_window = AnalyseCrypto()
 
+    app_icon = QSystemTrayIcon(QtGui.QIcon('static/icon.png'), parent=app)
+    app_icon.show()
+
     widget.addWidget(main_window)
     widget.setWindowTitle("Stock Assistant")
-    widget.setWindowIcon(QtGui.QIcon("static/icon.png"))
+    icon = QtGui.QIcon("static/icon.png")
+    app.setWindowIcon(icon)
+
     widget.addWidget(analyse_stocks_window)
     widget.addWidget(analyse_crypto_window)
     widget.showMaximized()
