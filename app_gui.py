6bddf8143ede484fa3e776766b9c726e542e858a
Syrnik Konrad <syrnikkonrad@gmail.com>
2022-04-20 13:24:22 +0200

    Add functionality for crypto portfolio

--- app_gui.py
+++ app_gui.py
@@ -43,6 +43,9 @@ class Home(QMainWindow):
         self.create_portfolio_button.clicked.connect(self.go_to_portfolio_form)
         self.analyse_portfolio_button.clicked.connect(self.go_to_analyse_portfolio)
         self.edit_portfolio_button.clicked.connect(self.go_to_portfolio_edit)
+        self.create_portfolio_crypto_button.clicked.connect(self.go_to_portfolio_form_crypto)
+        self.edit_portfolio_crypto_button.clicked.connect(self.go_to_portfolio_edit_crypto)
+
 
     def go_to_analyse_stocks(self):
         widget.setCurrentIndex(widget.currentIndex() + 1)
@@ -61,6 +64,12 @@ class Home(QMainWindow):
 
     def go_to_analyse_portfolio(self):
         widget.setCurrentIndex(widget.currentIndex() + 6)
+
+    def go_to_portfolio_form_crypto(self):
+        widget.setCurrentIndex(widget.currentIndex() + 10)
+
+    def go_to_portfolio_edit_crypto(self):
+        widget.setCurrentIndex(widget.currentIndex() + 11)
 
 class ChartWindow(QMainWindow):
 
@@ -417,6 +426,48 @@ class PortfolioForm(QMainWindow):
         m.setStandardButtons(QMessageBox.Ok)
         m.exec()
 
+
+class PortfolioFormCrypto(PortfolioForm):
+
+    cryptos = {}
+
+    def __init__(self):
+        super().__init__()
+
+        # read the window layout from file
+        loadUi("static/portfolio_form_crypto.ui", self)
+
+        # move to home window after clicking a button
+        self.back_button.clicked.connect(self.go_to_home)
+
+        # add, delete, clear elements in portfolio form
+        self.add_button.clicked.connect(self.add_it)
+        self.delete_it_button.clicked.connect(self.delete_it)
+        self.clear_button.clicked.connect(self.clear)
+
+        # update plot
+        self.add_button.clicked.connect(self.show_pie_plot)
+        self.delete_it_button.clicked.connect(self.show_pie_plot)
+        self.clear_button.clicked.connect(self.show_pie_plot)
+
+        # fill combobox with data from static csv file
+        self.read_csv_file('static/cryptos.csv', PortfolioFormCrypto.cryptos)
+        self.fill_combo_box(PortfolioFormCrypto.cryptos, self.comboBox_3)
+
+        self.browser = QtWebEngineWidgets.QWebEngineView(self)
+        self.vlayout.addWidget(self.browser)
+
+        # update latest company price
+        self.spinBox_4.valueChanged.connect(self.label_update)
+        self.comboBox_3.activated.connect(self.label_update)
+        self.label_5.setText(str(round(yf.Ticker(str(self.comboBox_3.currentText())).history(period='1d')['Close'][0]*(self.spinBox_4.value()), 2)))
+
+        self.save_button.clicked.connect(self.save_it)
+
+    def go_to_home(self):
+        widget.setCurrentIndex(widget.currentIndex() - 10)
+
+
 class PortfolioEdit(PortfolioForm):
 
     current_portfolio = ''
@@ -552,6 +603,51 @@ class PortfolioEdit(PortfolioForm):
         #TODO: deleting from database when save button clicked !!!
         self.my_table.removeRow(clicked)
         database_connector.delete_from(self.portfolio_combobox.currentText(), stock, date)
+
+class PortfolioEditCrypto(PortfolioEdit):
+
+    current_portfolio = ''
+    portfolio_length = 0
+
+    def __init__(self):
+        super().__init__()
+
+        # read the window layout from file
+        loadUi("static/portfolio_edit_crypto.ui", self)
+
+        # move to home window after clicking a button
+        self.back_button.clicked.connect(self.go_to_home)
+
+        self.fill_portfolio_combo_box()
+        self.load_button.clicked.connect(self.load_portfolio)
+        self.delete_portfolio_button.clicked.connect(self.delete_portfolio)
+
+        # add, delete, clear elements in portfolio form
+        self.add_button.clicked.connect(self.add_it)
+        self.delete_it_button.clicked.connect(self.delete_it)
+        self.clear_button.clicked.connect(self.clear)
+
+        # update plot
+        self.add_button.clicked.connect(self.show_pie_plot)
+        self.delete_it_button.clicked.connect(self.show_pie_plot)
+        self.clear_button.clicked.connect(self.show_pie_plot)
+
+        # fill combobox with data from static csv file
+        self.read_csv_file('static/cryptos.csv', PortfolioFormCrypto.cryptos)
+        self.fill_combo_box(PortfolioFormCrypto.cryptos, self.comboBox_3)
+
+        self.browser = QtWebEngineWidgets.QWebEngineView(self)
+        self.vlayout.addWidget(self.browser)
+
+        # update latest company price
+        self.spinBox_4.valueChanged.connect(self.label_update)
+        self.comboBox_3.activated.connect(self.label_update)
+        self.label_5.setText(str(round(yf.Ticker(str(self.comboBox_3.currentText())).history(period='1d')['Close'][0]*(self.spinBox_4.value()), 2)))
+
+        self.save_button.clicked.connect(self.save_it)
+
+    def go_to_home(self):
+        widget.setCurrentIndex(widget.currentIndex() - 11)
 
 
 class AnalysePortfolio(QMainWindow):
@@ -847,6 +943,8 @@ if __name__ == "__main__":
     portfolio_charts_window = PortfolioChart()
     correlation_charts_window = CorrelationWindow()
     sharpe_charts_window = SharpeWindow()
+    portfolio_form_crypto_window = PortfolioFormCrypto()
+    portfolio_edit_crypto_window = PortfolioEditCrypto()
 
 
     # add main window to stack
@@ -875,6 +973,8 @@ if __name__ == "__main__":
     widget.addWidget(portfolio_charts_window)
     widget.addWidget(correlation_charts_window)
     widget.addWidget(sharpe_charts_window)
+    widget.addWidget(portfolio_form_crypto_window)
+    widget.addWidget(portfolio_edit_crypto_window)
 
 
     # open in full screen
